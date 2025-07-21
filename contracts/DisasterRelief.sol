37314c4d5d9e533375b1d36335b4baf5c2798868
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-07-21 22:49:09 +0530

    Update DisasterRelief.sol

--- contracts/DisasterRelief.sol
+++ contracts/DisasterRelief.sol
@@ -1,1 +1,15 @@
-contract 
+contract DisasterRelief {
+  address public ngo;
+  address public owner;
+
+  constructor(address _ngo) {
+         owner = msg.sender;
+         ngo = _ngo;
+     }
+
+     function releaseFunds() public {
+         require(msg.sender == owner, "Not authorized");
+         payable(ngo).transfer(address(this).balance);
+     }
+     receive() external payable {}
+ }
